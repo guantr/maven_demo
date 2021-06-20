5465de0c53891b4ec7435d62ca0b96ccfeaee238
guantr <angel745279496@163.com>
2021-06-20 21:56:24 +0800

    first commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author xiyi
+ * @date 2021/6/20 - 17:10
+ */
+public class LoginTest {
+    @Test
+    public void loginSuccess(){
+        System.out.println("登陆成功");
+    }
+}
---
+++ src/test/java/testcases/RegisterTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author xiyi
+ * @date 2021/6/20 - 17:12
+ */
+public class RegisterTest {
+    @Test
+    public void registerSuccess(){
+        System.out.println("注册成功");
+    }
+}
